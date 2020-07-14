0793efe1653fcc082679bb7f82e645964938db79
Murilo Ribas <muriloribasss@hotmail.com>
2020-07-14 03:42:20 -0300

    updated eslint version; added cors.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import 'express-async-errors';
 import Router from './Router';
 import Database from './Database';
 import Auth from './Auth';
-
+import cors from 'cors'
 class App {
     constructor() {
         this.server   = express();
@@ -19,7 +19,24 @@ class App {
     }
 
     middlewares() {
+        this.server.use(cors())
         this.server.use(express.json());
+        this.server.use(function(req, res, next) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+            );
+            res.setHeader(
+                "Access-Control-Allow-Headers",
+                "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Access-Control-Request-Method"
+            );
+            if (req.method === "OPTIONS") {
+                return res.status(200).end();
+            }
+            next();
+        });
     }
 
     routes() {
